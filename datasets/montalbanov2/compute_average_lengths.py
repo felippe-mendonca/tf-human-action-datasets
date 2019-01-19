76d338f41837d63904e8bb9f5876128db6589ddc
Felippe <mendonca.felippe@gmail.com>
2019-01-19 16:01:33 -0200

    + simmetrical links average computation on montalbanov2 dataset

--- datasets/montalbanov2/compute_average_lengths.py
+++ datasets/montalbanov2/compute_average_lengths.py
@@ -29,20 +29,31 @@ def main(dataset_folder, output_folder):
     md = np.array(batch_avg_distances)
     w = np.expand_dims(np.array(batch_weights), axis=-1)
     avg_distances = np.sum(md * w, axis=0) / np.sum(w, axis=0)
-    avg_distances = avg_distances / np.linalg.norm(avg_distances)
 
-    avg_distances_df = pd.DataFrame(
-        data=np.expand_dims(avg_distances, axis=0), columns=[x[0] for x in MAIN_LINKS])
+    columns = [x[0] for x in MAIN_LINKS]
+    avg_distances_df = pd.DataFrame(data=np.expand_dims(avg_distances, axis=0), columns=columns)
+
+    def simmetrical_avg(df, link):
+        avg_value = (df[link + '_LEFT'] + df[link + '_RIGHT']) / 2.0
+        df[link + '_LEFT'] = df[link + '_RIGHT'] = avg_value
+
+    # compute average value for simmetrical links
+    for link in ['HIP', 'SHOULDER', 'ELBOW', 'WRIST']:
+        simmetrical_avg(avg_distances_df, link)
+
+    avg_distances_df = avg_distances_df / np.linalg.norm(avg_distances_df)
+
     filename = join(dataset_folder, 'average_distances.csv')
     avg_distances_df.to_csv(path_or_buf=filename, index=False)
 
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description="""This script computes the average distance
-of each main link throughout all train dataset. Afterwards, stores the normalized values on
-a file named average_distances.csv on the given dataset directory. The columns of the csv
-file are labeled with the first joint name, belonged to each tuple of MAIN_LINKS list.
-This list can be found on datasets.montalbanov2.base module.""")
+of each main link throughout all train dataset. As we have simmetrical links, average values 
+between left/righ side are computed afterwards. Normalized values are stored on a file named 
+average_distances.csv on the given dataset directory. The columns of the csv file are labeled 
+with the first joint name, belonged to each tuple of MAIN_LINKS list. This list can be found 
+on datasets.montalbanov2.base module.""")
     parser.add_argument(
         '--dataset',
         required=True,
