d16d1ea605373e3a690e1c3c3d1a90b76f51b351
Felippe <mendonca.felippe@gmail.com>
2019-01-23 02:34:48 -0200

    WIP: you dumb! Must use sigmoid activation instead of softmax for binary classification!

--- models/gesture_localization/model.py
+++ models/gesture_localization/model.py
@@ -6,8 +6,8 @@ from tensorflow.python.keras.layers import Activation
 
 def make_model(n_features, hidden_neurons):
     inputs = Input(shape=(n_features, ), name='features')
-    x = Dense(hidden_neurons, use_bias=True, kernel_initializer='random_uniform')(inputs)
+    x = Dense(hidden_neurons, use_bias=True, kernel_initializer='normal')(inputs)
     x = Activation(activation='relu')(x)
-    x = Dense(1, use_bias=True, kernel_initializer='random_uniform')(x)
-    outputs = Activation(activation='softmax')(x)
+    x = Dense(1, use_bias=True, kernel_initializer='normal')(x)
+    outputs = Activation(activation='sigmoid')(x)
     return Model(inputs, outputs)
