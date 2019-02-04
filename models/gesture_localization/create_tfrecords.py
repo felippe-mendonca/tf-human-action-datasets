fixed standarization on gesture_localization model and more.. - now standariaztion is made globally: mean and standard deviation of train dataset are computed during tfrecords creation. To do so, StatsRecords class were created on models.base.utils module. - changes on create_tfrecords of gesture_localization model: added mean and std computation, as well as argument to be able to choose which dataset parts are going to be created - new options of Dropout on gesture_localization model

models/gesture_localization/create_tfrecords.py
@@ -13,20 +13,23 @@ from utils.logger import Logger
 from datasets.tfrecords.features import bytes_feature, int64_feature
 from datasets.montalbanov2.base import Reader, PoseIterator
 from models.gesture_localization.encoding import DataEncoder
+from models.base.utils import StatsRecorder
 
 log = Logger(name='montalbanov2_create_tfrecords')
 
 
-def main(dataset_folder, output_folder):
+def main(dataset_folder, output_folder, parts):
 
     encoder = DataEncoder(dataset_folder)
 
     def make_tfrecords(dataset_part):
-        part_folder = join(output_folder, 'montalbanov2_tfrecords', dataset_part)
+        tfrecords_folder = join(output_folder, 'montalbanov2_tfrecords')
+        part_folder = join(tfrecords_folder, dataset_part)
         if exists(part_folder):
             rmtree(part_folder)
         makedirs(part_folder)
 
+        stats_recorder = StatsRecorder()
         reader = Reader(folder=join(dataset_folder, dataset_part))
         all_metadata = {}
         for sample, poses, labels in reader:
@@ -41,6 +44,7 @@ def main(dataset_folder, output_folder):
 
             metadata = {'samples': {'gesture': 0, 'not_gesture': 0}}
             encoder.reset()
+            sample_features = []
             for pose, label in zip(PoseIterator(poses), one_hot_labels):
                 if np.all(pose == 0.0):
                     continue
@@ -63,17 +67,37 @@ def main(dataset_folder, output_folder):
                 else:
                     metadata['samples']['not_gesture'] += 1
 
+                if dataset_part == 'train':
+                    sample_features.append(vec_features)
+
             writer.close()
             sys.stdout.flush()
             all_metadata[sample] = metadata
+            if dataset_part == 'train' and len(sample_features) > 0:
+                stats_recorder.update(np.vstack(sample_features))
+
             log.info('[Done] {}', sample)
 
         with open(part_folder + '.json', 'w') as f:
             json.dump(obj=all_metadata, fp=f, sort_keys=True, indent=2)
 
-    make_tfrecords('train')
-    make_tfrecords('validation')
-    make_tfrecords('test')
+        if dataset_part == 'train':
+            filename = join(tfrecords_folder, 'mean_features.csv')
+            df = pd.DataFrame(data=np.expand_dims(stats_recorder.mean, axis=0))
+            df.to_csv(path_or_buf=filename, index=False, header=False)
+
+            filename = join(tfrecords_folder, 'std_features.csv')
+            df = pd.DataFrame(data=np.expand_dims(stats_recorder.std, axis=0))
+            df.to_csv(path_or_buf=filename, index=False, header=False)
+
+    available_parts = ['train', 'validation', 'test']
+    for part in parts:
+        if part not in available_parts:
+            log.warn("Skipping part '{}'. Only {} are available.", part,
+                     ', '.join(available_parts))
+            continue
+
+        make_tfrecords(part)
 
 
 if __name__ == '__main__':
@@ -89,6 +113,14 @@ if __name__ == '__main__':
         type=str,
         default='.',
         help='Directory to save .tfrecord files.')
+    parser.add_argument(
+        '--dataset-parts',
+        type=str,
+        nargs='+',
+        default=['train', 'validation', 'test'],
+        help=
+        'Dataset parts that will have tfrecords file generated. Specify a list with train, validation and/or test.'
+    )
 
     args = parser.parse_args()
-    main(dataset_folder=args.dataset, output_folder=args.output)
+    main(dataset_folder=args.dataset, output_folder=args.output, parts=args.dataset_parts)

models/gesture_localization/encoding.py
@@ -11,13 +11,23 @@ from datasets.montalbanov2.base import CONNECTED_JOINTS, CONNECTED_BONES
 
 class DataEncoder:
     def __init__(self, dataset_folder, smooth_factor=0.8):
+        def raise_missing_file(filename):
+            raise Exception("File '{}' doesn't exist".format(filename))
+
         avg_distances_file = join(dataset_folder, 'average_distances.csv')
         if not exists(avg_distances_file):
-            raise Exception(
-                "'average_distances.csv' file doesn't exist on {} folder".format(dataset_folder))
+            raise_missing_file(avg_distances_file)
+        mean_features_file = join(dataset_folder, 'montalbanov2_tfrecords', 'mean_features.csv')
+        if not exists(mean_features_file):
+            raise_missing_file(mean_features_file)
+        std_features_file = join(dataset_folder, 'montalbanov2_tfrecords', 'std_features.csv')
+        if not exists(std_features_file):
+            raise_missing_file(std_features_file)
 
         self._alpha = smooth_factor
         self._avg_distances = pd.read_csv(avg_distances_file)
+        self._mean_features = np.squeeze(np.array(pd.read_csv(mean_features_file, header=None)))
+        self._std_features = np.squeeze(np.array(pd.read_csv(std_features_file, header=None)))
         self._main_joints = [JOINTS[x] for x in MAIN_JOINTS]
         self._main_joints_dict = {joint: pos for pos, joint in enumerate(MAIN_JOINTS)}
         all_joints = set(self._main_joints_dict.values())
@@ -39,6 +49,13 @@ class DataEncoder:
         self._uz = np.array([0, 0, 1])
 
         self._poses_s = deque(maxlen=2)
+
+    def standardize_features(self, features, *args):
+        features = (features - self._mean_features) / self._std_features
+        return (features, *args)
+    
+    def get_dataset_stats(self):
+        return self._mean_features, self._std_features
 
     def reset(self):
         self._poses_s.clear()
@@ -71,7 +88,7 @@ class DataEncoder:
             bending_angles, pairwise_distances
         ]
 
-        return np.hstack(map(self.normalize, vec_features))
+        return np.hstack(vec_features)
 
     def normalize_pose(self, pose):
         """
@@ -134,6 +151,3 @@ class DataEncoder:
         self._ux = self._ux - proj_x_y
         self._ux = self._ux / np.linalg.norm(self._ux)
         self._uz = np.cross(self._ux, self._uy)
-
-    def normalize(self, array):
-        return (array - array.mean()) / array.std()

models/gesture_localization/train.py
@@ -14,6 +14,7 @@ from tensorflow.python.keras.models import load_model
 
 from datasets.tfrecords.features import decode
 from models.gesture_localization.model import make_model
+from models.gesture_localization.encoding import DataEncoder
 from models.options.options_pb2 import GestureLocalizationOptions, Datasets, Optimizers
 from models.options.utils import load_options
 from models.base.utils import get_logs_dir, gen_model_name
@@ -49,17 +50,26 @@ def load_metadata(dataset_folder, dataset_type=None):
 def main(options_filename, model_file=None, weights=None, reset_lr=False):
     op = load_options(options_filename, GestureLocalizationOptions)
     dataset_name = Datasets.Name(op.dataset).lower()
-    dataset_folder = join(op.storage.datasets_folder, dataset_name,
-                          '{}_tfrecords'.format(dataset_name))
+    dataset_folder = join(op.storage.datasets_folder, dataset_name)
+    dataset_tfrecords_folder = join(dataset_folder, '{}_tfrecords'.format(dataset_name))
+
+    encoder = DataEncoder(dataset_folder=dataset_folder)
+    mean_data, std_data = encoder.get_dataset_stats()
+    mean_data, std_data = tf.constant(mean_data), tf.constant(std_data)
 
     def make_dataset(dataset_type):
         def make_one_hot_label(feature, label):
             return feature, tf.one_hot(label, 2)
 
-        folder = join(dataset_folder, dataset_type)
+        def standardize_feature(feature, label):
+            feature = (feature - mean_data) / std_data
+            return feature, label
+
+        folder = join(dataset_tfrecords_folder, dataset_type)
         files = tf.data.Dataset().list_files(join(folder, 'Sample*.tfrecords'))
         dataset = tf.data.TFRecordDataset(filenames=files)
         dataset = dataset.map(decode)
+        dataset = dataset.map(standardize_feature)
         dataset = dataset.map(make_one_hot_label)
         return dataset
 
@@ -67,9 +77,12 @@ def main(options_filename, model_file=None, weights=None, reset_lr=False):
     validation_dataset = make_dataset('validation')
     test_dataset = make_dataset('test')
 
-    train_gesture, train_not_gesture = load_metadata(dataset_folder, 'train')
-    validation_gesture, validation_not_gesture = load_metadata(dataset_folder, 'validation')
-    test_gesture, test_not_gesture = load_metadata(dataset_folder, 'test')
+    def part_metadata(part):
+        return load_metadata(dataset_tfrecords_folder, part)
+
+    train_gesture, train_not_gesture = part_metadata('train')
+    validation_gesture, validation_not_gesture = part_metadata('validation')
+    test_gesture, test_not_gesture = part_metadata('test')
     steps_per_epoch = int((train_gesture + train_not_gesture) / op.training.batch_size)
     validation_steps = int((validation_gesture + validation_not_gesture) / op.training.batch_size)
 